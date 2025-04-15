ee43b7107face763c8c26ed2dafae4949b0751cc
heeyeop <ssucgoo@gmail.com>
2025-04-15 12:09:13 +0900

    Initial commit

---
+++ test/payment-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PaymentAuth from '../lib/payment-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/payment-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PaymentAuth.PaymentAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
